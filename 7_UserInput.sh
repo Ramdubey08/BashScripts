da210489e8d912a21756e437c63d9735068f506a
Ramdubey08 <work.ramdubey@gmail.com>
2026-07-08 11:39:27 +0000

    Learned about condition statement (if else)

---
+++ 7_UserInput.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Enter Your skills:"
+read SKILL
+
+echo "Your $SKILL skill is in high Demand in the IT Industry."
+
+read -p 'Usearname': USR
+read -sp 'Password': pass
+
+echo
+
+echo "Login Successfull: Welcome USER $USR."
+
+
+
---
+++ 9_ifElse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+read -p "Enter a number: " NUM
+echo 
+
+if [ $NUM -gt 100 ]
+then 
+	echo "We have entered in IF block. "
+	sleep 3
+	echo "Your Number is greater than 100. "
+	echo
+	date
+
+else 
+	echo "Your number is less than 100. "
+fi 
+
+echo "Script exection completed successfully. "
+
+
